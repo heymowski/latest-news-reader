b9a3dfb11e657bd757868c96ca2450fc49a98d6c
Juan Jose Juan Heymowski <juan@heymowski.es>
2018-08-17 20:09:02 +0200

    Added ServiceProvider

---
+++ src/LatestNewsReaderServceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Heymowski\LatestNewsReader;
+
+use Illuminate\Support\ServiceProvider;
+
+class LatestNewsReaderServceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+    }
+}
