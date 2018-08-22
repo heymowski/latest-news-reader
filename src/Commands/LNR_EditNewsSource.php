88e073b9945947a14905ce2f8f0b029fe74698e8
Juan Jose Juan Heymowski <juan@heymowski.es>
2018-08-22 11:03:12 +0200

    News Sources Various

--- src/Commands/LNR_EditNewsSource.php
+++ src/Commands/LNR_EditNewsSource.php
@@ -2,6 +2,7 @@
 
 namespace Heymowski\LatestNewsReader\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
 use Heymowski\LatestNewsReader\Models\NewsSource;
 
@@ -39,23 +40,67 @@ class LNR_EditNewsSource extends Command
         // Hello
         $this->info('Hello, do you want to edit a source?');
 
-        $this->info('these are the available sources:');
+        // Print Sources Table
+        $this->printSourcesTable('these are the available sources:');
+
+        // Get Source Id
+        $newsSourceIdToEdit = $this->ask('Insert the id of the source you want to edit');
+
+        // Try to get model for this id
+        try {
+            $newsSourceToEdit = NewsSource::find($newsSourceIdToEdit);
+
+            // Correct model
+            if ($this->confirm('('.$newsSourceToEdit->name.') Is this the source you want to edit?')) {
+
+                // Name
+                $this->info('Name : '.$newsSourceToEdit->name);
+                $this->info('-----------------------------');
+                // Change name?
+                if ($this->confirm('Do you wish to change the name?')) {
+                    $newsSourceToEdit->name = $this->ask('Insert new Name');
+                    $newsSourceToEdit->slug = str_slug($newsSourceToEdit->name, '-');
+                }
+
+                // URL
+                $this->info('Url : '.$newsSourceToEdit->url);
+                $this->info('-----------------------------');
+                // Change Url?
+                if ($this->confirm('Do you wish to change the url?')) {
+                    $newsSourceToEdit->url = $this->ask('Insert new Url');
+                }
+
+                // Save Changes
+                $newsSourceToEdit->save();
+
+                // Print results
+                $this->printSourcesTable('these are the results:');
+
+                exit;
+            }
+
+            $this->info('-----------------------------');
+            $this->info('No changes have been made, bye.');
+        } catch (Exception $e) {
+        }
+
+        // If Ther is no model for id
+        $this->info('Wrong Id');
+    }
+
+    /**
+     * Print Table with Sources.
+     */
+    protected function printSourcesTable($title)
+    {
+        $this->info('-----------------------------');
+
+        $this->info($title);
 
         $headers = ['ID', 'Name', 'Slug', 'Url'];
 
         $newsSources = NewsSource::all(['id', 'name', 'slug', 'url'])->toArray();
 
         $this->table($headers, $newsSources);
-
-        $newsSourceIdToEdit = $this->ask('Insert the id of the source you want to edit');
-
-        $newsSourceToEdit = NewsSource::find($newsSourceIdToEdit);
-
-        if ($this->confirm('('.$newsSourceToEdit->name.') Is this the source you want to edit?')) {
-            $this->info('OK');
-            exit;
-        }
-
-        $this->info('KO');
     }
 }
--- src/Commands/LNR_RemoveNewsSource.php
+++ src/Commands/LNR_RemoveNewsSource.php
@@ -2,7 +2,9 @@
 
 namespace Heymowski\LatestNewsReader\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
+use Heymowski\LatestNewsReader\Models\NewsSource;
 
 class LNR_RemoveNewsSource extends Command
 {
@@ -35,5 +37,59 @@ class LNR_RemoveNewsSource extends Command
      */
     public function handle()
     {
+        // Hello
+        $this->info('Hello, do you want to remove a source?');
+
+        // Print Sources Table
+        $this->printSourcesTable('these are the available sources:');
+
+        // Get Source Id
+        $newsSourceIdToRemove = $this->ask('Insert the id of the source you want to remove');
+
+        // Try to get model for this id
+        try {
+            $newsSourceToRemove = NewsSource::find($newsSourceIdToRemove);
+
+            // Correct model
+            if ($this->confirm('('.$newsSourceToRemove->name.') Is this the source you want to remove?')) {
+
+                // Removing
+                $this->info('Removing : '.$newsSourceToRemove->name);
+                $this->info('-----------------------------');
+
+                // Save Changes
+                $newsSourceToRemove->delete();
+
+                $this->info('Done');
+
+                // Print results
+                $this->printSourcesTable('these are the results:');
+
+                exit;
+            }
+
+            $this->info('-----------------------------');
+            $this->info('No changes have been made, bye.');
+        } catch (Exception $e) {
+        }
+
+        // If Ther is no model for id
+        $this->info('Wrong Id');
+    }
+
+    /**
+     * Print Table with Sources.
+     */
+    protected function printSourcesTable($title)
+    {
+        $this->info('-----------------------------');
+
+        $this->info($title);
+
+        $headers = ['ID', 'Name', 'Slug', 'Url'];
+
+        $newsSources = NewsSource::all(['id', 'name', 'slug', 'url'])->toArray();
+
+        $this->table($headers, $newsSources);
     }
 }
