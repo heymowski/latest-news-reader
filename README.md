db308b0db950d38d131ed5a8f45d7e0e46121d53
Juan Jose Juan Heymowski <juan@heymowski.es>
2018-08-29 01:49:58 +0200

    Changed Readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # latest-news-reader
 Latest News Reader (Simple Pie Based)
+
+1 - Migrate DB
+
+	Artisan Migrate
+
+2 - Add News Source
+
+	Artisan LNR-Sources:AddNewsSource
+
+3 - Load Feeds to db
+
+	Artisan LNR-Items:ReadAll
--- src/Commands/Items/LNR_ReadAll.php
+++ src/Commands/Items/LNR_ReadAll.php
@@ -5,8 +5,6 @@ namespace Heymowski\LatestNewsReader\Commands\Items;
 use Illuminate\Console\Command;
 use Heymowski\LatestNewsReader\Reader;
 use Heymowski\LatestNewsReader\Models\NewsSource;
-use Heymowski\LatestNewsReader\Models\NewsItem;
-use Carbon\Carbon;
 
 class LNR_ReadAll extends Command
 {
@@ -46,64 +44,6 @@ class LNR_ReadAll extends Command
 
         foreach ($newsSources as $newsSource) {
             $reader->processFeedItems($newsSource);
-            //$readedSource = $reader->read($newsSource->url);
-
-            // $items = $readedSource->get_items();
-
-            // foreach ($items as $item) {
-            //     $title = $item->get_title();
-            //     $description = $item->get_description();
-            //     $content = $item->get_content();
-            //     $category = $item->get_category();
-
-            //     $categories = '';
-            //     if ($item->get_categories() != null) {
-            //         foreach ($item->get_categories() as $cat) {
-            //             $categories .= $cat->name;
-            //         }
-            //     }
-
-            //     $author = $item->get_author()->name;
-            //     $contributor = $item->get_contributor();
-
-            //     $contributors = '';
-            //     if ($item->get_contributors() != null) {
-            //         foreach ($item->get_contributors() as $cont) {
-            //             $contributors .= $cont->name;
-            //         }
-            //     }
-            //     $authors = '';
-            //     if ($item->get_authors() != null) {
-            //         foreach ($item->get_authors() as $auth) {
-            //             $authors .= $auth->name;
-            //         }
-            //     }
-
-            //     $copyright = $item->get_copyright();
-            //     $date = new Carbon($item->get_date());
-            //     $updated_date = new Carbon($item->get_updated_date());
-            //     $link = $item->get_link();
-            //     $source = $item->get_source();
-
-            //     $newsItem = new NewsItem([
-            //         'title' => $title,
-            //         'description' => $description,
-            //         'content' => $content,
-            //         'category' => $category,
-            //         'categories' => $categories,
-            //         'author' => $author,
-            //         'contributor' => $contributor,
-            //         'contributors' => $contributors,
-            //         'authors' => $authors,
-            //         'copyright' => $copyright,
-            //         'date' => $date,
-            //         'updated_date' => $updated_date,
-            //         'link' => $link,
-            //         'source' => $source,
-            //     ]);
-
-            //     $newsSource->newsItems()->save($newsItem);
-            // }
         }
     }
 }
